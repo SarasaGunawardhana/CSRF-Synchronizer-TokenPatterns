82d7d302f0617c48c78e8dead03dd2431c7f3808
SarasaGunawardhana <it14078842@my.sliit.lk>
2018-07-24 02:17:21 +0530

    create index file and add form

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SSD</title>
+	</head>
+	<body>
+		<form action="action.php" method="post">
+			<div class="login">
+				<strong>Login</strong>
+					<div class="credentials">
+							Username: <input type="text" name="username" value="sarasa">
+							Password: <input type="password" name="password" value="password">
+					</div>
+					<input type="Submit" value="Login">
+
+			</div>
+		</form>
+	</body>
+</html>
